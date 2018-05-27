7e429147d3e5ee443087c08f1d13bb1e81d9372d
Tommi Kivela <tommi.kivela@pihlajalinna.fi>
2018-05-27 11:11:02 +0300

    Moved prettier settings to .prettierrc, bumped dependency versions.

--- src/StateAndRouteProvider.tsx
+++ src/StateAndRouteProvider.tsx
@@ -1,4 +1,5 @@
 import { createBrowserHistory } from 'history'
+import { configure } from 'mobx'
 import { Provider } from 'mobx-react'
 import { RouterStore, syncHistoryWithStore } from 'mobx-react-router'
 import * as React from 'react'
@@ -7,6 +8,8 @@ import { Route } from 'react-router-dom'
 
 import store from './stores'
 import App from './views/App'
+
+configure({ enforceActions: true })
 
 export default () => {
   const browserHistory = createBrowserHistory()
