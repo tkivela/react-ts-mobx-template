97ac7303138eda65f36928e0c536a78f5f074a63
Tommi Kivela <tommi.kivela@pihlajalinna.fi>
2018-09-12 22:26:03 +0300

    Removed unused require.

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,8 @@
-const { injectBabelPlugin } = require('react-app-rewired');
-const rewireMobX = require('react-app-rewire-mobx');
+const rewireMobX = require('react-app-rewire-mobx')
 
 module.exports = function override(config, env) {
   // use the MobX rewire
-  config = rewireMobX(config, env);
+  config = rewireMobX(config, env)
 
   return config
 }
