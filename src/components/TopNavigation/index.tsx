8338b9ddaa0a16caf0cb8cd6b17d03e5c6d79c58
Tommi Kivela <tommi.kivela@pihlajalinna.fi>
2018-09-12 20:24:21 +0300

    Removed svg logo.

--- src/components/TopNavigation/index.tsx
+++ src/components/TopNavigation/index.tsx
@@ -3,31 +3,16 @@ import { observer } from 'mobx-react'
 import * as React from 'react'
 
 import CustomButton, { blueButton, darkButton, orangeButton } from '../../components/CustomButton'
-import logo from './logo.svg'
 
 const appStyles = css`
   font-family: sans-serif;
   text-align: center;
 
-  img {
-    animation: App-logo-spin infinite 20s linear;
-    height: 80px;
-  }
-
   header {
-    background-color: #222;
-    height: 150px;
+    background-color: #d4cfcf;
+    height: 80px;
     padding: 20px;
     color: white;
-  }
-
-  @keyframes App-logo-spin {
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-    }
   }
 `
 
@@ -59,7 +44,6 @@ export default class extends React.Component<IAppProps> {
         />
         <p />
         <header>
-          <img src={logo} alt="logo" />
           <div>
             <CustomButton
               title="Add a latin note"
