e994faf8f4e8e91dec43ac297ef7ce2480dc39ef
Tommi Kivela <tommi.kivela@pihlajalinna.fi>
2018-09-13 07:05:53 +0300

    Removed unnecessary comment line.

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,6 @@
 const rewireMobX = require('react-app-rewire-mobx')
 
 module.exports = function override(config, env) {
-  // use the MobX rewire
   config = rewireMobX(config, env)
-
   return config
 }
